68dd214d607296ae596ba6ab90195e2d8518bb2e
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-04-12 15:23:05 -0500

    first commit

---
+++ ROC/roc-day1/src/basics/Hello.java
@@ -0,0 +1,17 @@
+package basics;
+
+public class Hello {
+	// These are code comments
+	
+	/*
+	 * Comments are not 'read' by the compiler
+	 * they are used as a means to save messages 
+	 * or notes for yourself or other developers
+	 * 
+	 */
+	
+	//public static void main(String[] args) {
+		// System.out.println("Hello World!");
+	 //}
+
+}
---
+++ ROC/roc-day1/src/basics/PrimativeTypes.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class PrimativeTypes {
+	/*
+	 * A variable is a construct that is used
+	 * to hold onto some information.
+	 * 
+	 * In Java, we can specify two types of variables 
+	 */
+  byte b;
+  short s;
+  int i;
+  long l;
+  float f;
+  double d;
+  boolean bool;
+  char c;
+}
