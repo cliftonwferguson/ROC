0fd184099f43b75031bb5c3efc600eb8782dc747
Clifton Ferguson <clifton.ferguson7@gmail.com>
2021-04-12 16:11:10 -0500

    updating notes

---
+++ ROC/roc-day1/src/basics/Casting.java
@@ -0,0 +1,29 @@
+package basics;
+
+public class Casting {
+	/*
+	 * When casting, Java will implicitly 
+	 * upcast (convert from smaller to larger)
+	 * 
+	 */
+	public static void main(String[] args) {
+		int num = 65;
+		double decimal = num;
+		
+		double decimal_2 = 65.0;
+		int num_2 = (int) decimal_2;
+		
+		char letter = (char)num;
+		System.out.println(letter);
+		
+		/*
+		 * When converting between datatypes
+		 * the binary values are truncated
+		 * which can lead to strange representations
+		 * for numbers
+		 */
+		byte b = (byte)3255;
+		System.out.println(b);
+	}
+
+}
--- ROC/roc-day1/src/basics/PrimativeTypes.java
+++ ROC/roc-day1/src/basics/PrimativeTypes.java
@@ -7,11 +7,11 @@ public class PrimativeTypes {
 	 * 
 	 * In Java, we can specify two types of variables 
 	 */
-  byte b;
-  short s;
-  int i;
-  long l;
-  float f;
+  byte b = 127; //whole number 8 bits
+  short s = 12345; //whole number that is 16 bits
+  int i = 1_000_000; //whole number 32 bits
+  long l = 1_000_000_000l;  //whole number 64 bits
+  float f = 1_000_000;
   double d;
   boolean bool;
   char c;
